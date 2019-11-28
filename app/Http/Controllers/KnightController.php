e813b7b97d3d4e48b105e5f86a6806a238730f6d
hunglui31 <nguyenhungtran1297@gmail.com>
2019-11-28 11:01:02 +0700

    Update KnightController.php

--- app/Http/Controllers/KnightController.php
+++ app/Http/Controllers/KnightController.php
@@ -136,7 +136,7 @@ class KnightController extends Controller
             $caseDetail->save();
 
             $case->status = 1;
-            $case->confirmKnightId = $knightId;
+            $case->knightConfirmId = $knightId;
             $case->save();
 
             // dd($knightId);
