63fe30b74f10b7070b153aead4a12590be0ffd8a
hunglui31 <nguyenhungtran1297@gmail.com>
2019-11-28 11:10:38 +0700

    Update KnightController.php

--- app/Http/Controllers/KnightController.php
+++ app/Http/Controllers/KnightController.php
@@ -226,11 +226,11 @@ class KnightController extends Controller
     }
 
     public function confirmCase($knightId, $caseId){
-        $caseDetail = CaseDetail::where('caseId', $caseId)
-                                ->first();
-        // dd($caseDetail);
-        if(isset($caseDetail)){
-            $case = Cases::find($caseId);
+        // $caseDetail = CaseDetail::where('caseId', $caseId)
+        //                         ->first();
+        // // dd($caseDetail);
+        $case = Cases::find($caseId);
+        if(isset($case)){
             // dd($case);
             $case->knightConfirmId = $knightId;
             $case->save();
