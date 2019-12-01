473927b1fb31a8fb2147e1bcecdb54e3aecd6763
hunglui31 <nguyenhungtran1297@gmail.com>
2019-12-01 11:50:50 +0700

    gfd
    dfg

--- app/Http/Controllers/UserReportController.php
+++ app/Http/Controllers/UserReportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\UserReport;
+use App\Users;
 use Illuminate\Http\Request;
 
 class UserReportController extends Controller
@@ -10,8 +11,17 @@ class UserReportController extends Controller
     private $ROLE_KNIGHT = 2;
     private $ROLE_CITIZEN = 1;
 
-    public function reportUser($userId, $reason, $reporterId='', $caseId = ''){
+    public function reportUser(){
+        
+        $resultCode = 200;
+        $message = 'FAIL';
+        $data = [];
+
         $userReport = new UserReport();
+
+        $json = json_decode(file_get_contents('php://input'), true);
+        $userId = str_replace('+84','0',$json['userId']);
+        $reason = $json['reason'];
         $user = Users::find($userId);
         // if($user->role == $this->ROLE_KNIGHT){
 
@@ -20,12 +30,18 @@ class UserReportController extends Controller
         // }
         $userReport->userId = $userId;
         $userReport->reason = $reason;
-        if(!empty($reporterId)){
-            $userReport->reporterId = $reporterId;
+        if(isset($json['reporterId'])){
+            $userReport->reporterId = $json['reporterId'];
         }
-        if(!empty($caseId)){
-            $userReport->caseId = $caseId;
+        if(!empty($json['caseId'])){
+            $userReport->caseId = $json['reporterId'];
         }
-        $userReport->save();
+        $result = $userReport->save();
+        if($result){
+            $resultCode = 200;
+            $message = 'SUCCESS';
+            $data = $userReport;
+        }   
+        return $this->returnAPI($resultCode,$message,$data);
     }
 }
--- database/migrations/2019_11_30_143646_create_user_reports_table.php
+++ database/migrations/2019_11_30_143646_create_user_reports_table.php
@@ -15,11 +15,11 @@ class CreateUserReportsTable extends Migration
     {
         Schema::create('user_reports', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->timestamps();
             $table->string('userId');
             $table->text('reason');
             $table->string('reporterId')->nullable();
             $table->string('caseId')->nullable();
+            $table->timestamps();
             // $table->foreign('userId')->references('id')->on('users');
             // $table->foreign('reporterId')->references('id')->on('users');
         });
