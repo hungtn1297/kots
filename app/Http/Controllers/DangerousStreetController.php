322ad1245e588c4810f8db5293dc7631bbfe6941
hunglui31 <nguyenhungtran1297@gmail.com>
2019-12-25 22:08:40 +0700

    RELEASE

--- app/Http/Controllers/DangerousStreetController.php
+++ app/Http/Controllers/DangerousStreetController.php
@@ -67,6 +67,7 @@ class DangerousStreetController extends Controller
         $dangerousStreet->endLatitude = $latEnd;
         $dangerousStreet->description = $des;
         $dangerousStreet->save();
+        // dd($dangerousStreet);
     }
 
     public function unsetDS(Request $request){
@@ -126,10 +127,12 @@ class DangerousStreetController extends Controller
                 $latLongArr = $dsStreet[$address];
                 array_push($latLongArr, $latLng);
                 $dsStreet[$address] = $latLongArr;
-                if(count($dsStreet[$address]) >= 3){
+                if(count($dsStreet[$address]) >= 2){
+                    // dd($dsStreet[$address]);
                     $latLng = $this->getLatLongStartEnd($dsStreet[$address]);
                     // dd($latLng);
                     if(!empty($latLng)){
+                        // dd($address);
                         $ds = DangerousStreet::where('description',$address)->first();
                         if(count($ds) > 0){
                             $ds->expiredDate = Carbon::now()->addDay(7);
@@ -144,7 +147,7 @@ class DangerousStreetController extends Controller
                     }
                 }
             }
-            
+
         }
         // dd($dsStreet);
     }
